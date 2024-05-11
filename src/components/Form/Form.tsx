89cdf2d77e05aebdb6770857fb4a0c5b055f92f0
ManuelCalam <jmanuelc369@gmail.com>
2024-05-10 20:12:20 -0600

    Login con credenciales del back

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,15 +1,20 @@
 import { ChangeEvent, useState } from 'react';
-import './Form.css'
+import './Form.css';
+
+const API_URL = "http://localhost:3010/";
 
 const loginData = {
     email: 'jmanuelc369@gmail.com',
     password: '12345'
 }
 
+
+
 function Form(){
     const [email, setEmail] = useState<string>("");
     const [password, setPassword] = useState<string>("");
     const [showData, setShowData] = useState<boolean>(false);
+    const [user, setUser] = useState<any>(null);
 
     // const handleInputChange = (stateUpdate) => {
     //     return (event) => {
@@ -27,20 +32,49 @@ function Form(){
     }
 
     const handleOnClick = () =>{
-        if(!showData){
-            if(email === loginData.email && password === loginData.password){
-                setShowData(true);
-            }
-            else{
-                alert("Credenciales incorrectas.");
+        logIn({email, password});
+
+        // if(!showData){
+        //     if(email === loginData.email && password === loginData.password){
+        //         setShowData(true);
+        //     }
+        //     else{
+        //         alert("Credenciales incorrectas.");
+        //     }
+
+        // }
+        // else{
+        //     setShowData(!showData)
+            
+        // }
+        
+    }
+
+    const logIn = async ({email, password}: {email: string, password: string}) => {
+        try{
+            const response = await fetch(`${API_URL}api/v1/auth/login`, {
+                method : 'POST',
+                headers: {
+                    "Content-Type" : "application/json",
+                },
+                body: JSON.stringify({email, password}),
+            });
+
+            if(response.status === 200){
+                const data = await response.json();
+                console.log(data);
+                setUser(data);
+            }else{
+                alert("Usuario o contraseña incorrectos");
+
             }
 
+    
+    
+        } catch(error){
+            console.log(error);
         }
-        else{
-            setShowData(!showData)
-            
-        }
-        
+    
     }
 
     return (
